3d682b88e13c11cb8bfbd8597a802d6fe7a62f12
Vikash Patel <pvickey663@gmail.com>
2022-01-08 15:47:07 +0530

    domModification

---
+++ domModification.js
@@ -0,0 +1,12 @@
+//attribute >>
+//   <input type="text"></input> here type is attribute //everything in light blue color
+// selector
+// querySelector and querySelectorAll
+// getEleementByid and getElementByClassName
+//getAttribute
+const link=document.querySelector("a")
+console.log(link.getAttribute("href"));
+console.log(link.setAttribute("href","https://google.com"));
+
+const styling=document.querySelector()
+
