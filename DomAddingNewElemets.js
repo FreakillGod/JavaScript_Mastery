fa9b298803006de633c78d7338412f37b38ff68d
Vikash Patel <pvickey663@gmail.com>
2022-01-10 16:09:01 +0530

    adding element

---
+++ DomAddingNewElemets.js
@@ -0,0 +1,13 @@
+//classList
+const sectionClass=document.querySelector(".todosection")
+console.log(sectionClass)
+console.log(sectionClass.classList);
+
+//adding class using javacript
+
+sectionClass.classList.toggle('bg-dark');  //dont put . seriously i wasted so much of my time
+
+//Adding html element
+//bad way
+
+// const newList=document.querySelector(".")
